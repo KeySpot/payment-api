2e96c6f4dd43627097b28db2772c284e26ceb870
carlschader <carlschader@gmail.com>
2021-08-02 14:20:25 -0600

    path /webhook/webhook -> /webhook

--- src/webhook.js
+++ src/webhook.js
@@ -4,65 +4,68 @@ const process = require('process');
 const express = require('express');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const fetch = require('node-fetch');
+const { default: getToken } = require('./token');
 
 const router = express.Router();
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
-router.post("/webhook", async (req, res) => {
-  let data;
-  let eventType;
-  // Check if webhook signing is configured.
-  if (webhookSecret) {
-    // Retrieve the event by verifying the signature using the raw body and secret.
-    let event;
-    let signature = req.headers["stripe-signature"];
+router.post('/', async (req, res) => {
+    const token = await getToken();
 
-    try {
-      event = stripe.webhooks.constructEvent(
-        req.body,
-        signature,
-        webhookSecret
-      );
-    } catch (err) {
-      console.log(`⚠️  Webhook signature verification failed.`);
-      return res.sendStatus(400);
-    }
-    // Extract the object from the event.
-    data = event.data;
-    eventType = event.type;
-  } else {
-    // Webhook signing is recommended, but if the secret is not configured in `config.js`,
-    // retrieve the event data directly from the request body.
-    data = req.body.data;
-    eventType = req.body.type;
-  }
+    let data;
+    let eventType;
+    // Check if webhook signing is configured.
+    if (webhookSecret) {
+        // Retrieve the event by verifying the signature using the raw body and secret.
+        let event;
+        let signature = req.headers["stripe-signature"];
 
-  switch (eventType) {
-      case 'checkout.session.completed':
-        // Payment is successful and the subscription is created.
-        // You should provision the subscription and save the customer ID to your database.
-        console.log(eventType)
-        console.log(data)
-        break;
-      case 'invoice.paid':
-        // Continue to provision the subscription as payments continue to be made.
-        // Store the status in your database and check when a user accesses your service.
-        // This approach helps you avoid hitting rate limits.
-        console.log(eventType)
-        console.log(data)
-        break;
-      case 'invoice.payment_failed':
-        // The payment failed or the customer does not have a valid payment method.
-        // The subscription becomes past_due. Notify your customer and send them to the
-        // customer portal to update their payment information.
-        console.log(eventType)
-        console.log(data)
-        break;
-      default:
-      // Unhandled event type
+        try {
+        event = stripe.webhooks.constructEvent(
+            req.body,
+            signature,
+            webhookSecret
+        );
+        } catch (err) {
+        console.error(`⚠️  Webhook signature verification failed.`);
+        return res.sendStatus(400);
+        }
+        // Extract the object from the event.
+        data = event.data;
+        eventType = event.type;
+    } else {
+        // Webhook signing is recommended, but if the secret is not configured in `config.js`,
+        // retrieve the event data directly from the request body.
+        data = req.body.data;
+        eventType = req.body.type;
     }
 
-  res.sendStatus(200);
+    switch (eventType) {
+        case 'checkout.session.completed':
+            // Payment is successful and the subscription is created.
+            // You should provision the subscription and save the customer ID to your database.
+            console.log(eventType)
+            console.log(data)
+            break;
+        case 'invoice.paid':
+            // Continue to provision the subscription as payments continue to be made.
+            // Store the status in your database and check when a user accesses your service.
+            // This approach helps you avoid hitting rate limits.
+            console.log(eventType)
+            console.log(data)
+            break;
+        case 'invoice.payment_failed':
+            // The payment failed or the customer does not have a valid payment method.
+            // The subscription becomes past_due. Notify your customer and send them to the
+            // customer portal to update their payment information.
+            console.log(eventType)
+            console.log(data)
+            break;
+        default:
+        // Unhandled event type
+        }
+
+    res.sendStatus(200);
 });
 
 module.exports = router;
